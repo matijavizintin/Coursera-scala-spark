15cc3080010f27cd8d84bbd5b750018f714f871a
matijavizintin <matija.vizintin@gmail.com>
2017-03-23 09:05:05 +0100

    minor refactor sql suite

--- src/test/scala/week4/DataSuite.scala
+++ src/test/scala/week4/DataSuite.scala
@@ -1,28 +1,22 @@
 package week4
 
-import org.apache.spark.{SparkConf, SparkContext}
+import helpers.Common._
+import helpers.Generator._
 import org.junit.runner.RunWith
+import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-import week4.helpers.Generator._
-import week4.helpers.Sparky
 
 /**
   * Created by matijav on 20/03/2017.
   */
 @RunWith(classOf[JUnitRunner])
-class DataSuite extends Sparky {
-    val conf: SparkConf = new SparkConf().setAppName("example").setMaster("local[4]")
-    var sc: SparkContext = new SparkContext(conf)
-
+class DataSuite extends FunSuite {
     private val elements = 100 * 1000
 
-    private val demographicsList = generateDemographic(elements)
-    private val financesList = generateFinances(elements)
+    private val demographics = sc.parallelize(generateDemographic(elements)).map(x => (x.id, x))
+    private val finances = sc.parallelize(generateFinances(elements)).map(x => (x.id, x))
 
     test("test 1") {
-        val demographics = sc.parallelize(demographicsList).map(x => (x.id, x))
-        val finances = sc.parallelize(financesList).map(x => (x.id, x))
-
         val eligible = demographics.join(finances).filter {
             case (_, (d, f)) => d.country == "Switzerland" && f.hasDebt && f.hasFinancialDependents
         }.count
@@ -31,9 +25,6 @@ class DataSuite extends Sparky {
     }
 
     test("test 2") {
-        val demographics = sc.parallelize(demographicsList).map(x => (x.id, x))
-        val finances = sc.parallelize(financesList).map(x => (x.id, x))
-
         val eligible = demographics.filter {
             case (_, d) => d.country == "Switzerland"
         }.join(finances.filter {
@@ -44,9 +35,6 @@ class DataSuite extends Sparky {
     }
 
     test("test 3") {
-        val demographics = sc.parallelize(demographicsList).map(x => (x.id, x))
-        val finances = sc.parallelize(financesList).map(x => (x.id, x))
-
         val eligible = demographics.cartesian(finances).filter{
             case ((id1, _), (id2, _)) => id1 == id2
         }.filter{
--- src/test/scala/week4/SqlSuite.scala
+++ src/test/scala/week4/SqlSuite.scala
@@ -1,23 +1,21 @@
 package week4
 
+import helpers.Common._
+import helpers.Generator._
 import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.{SparkConf, SparkContext}
 import org.junit.runner.RunWith
+import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-import week4.helpers.Generator._
-import week4.helpers.Sparky
 
 /**
   * Created by matijav on 20/03/2017.
   */
 @RunWith(classOf[JUnitRunner])
-class SqlSuite extends Sparky {
-    val conf: SparkConf = new SparkConf().setAppName("example").setMaster("local[4]")
-    var sc: SparkContext = new SparkContext(conf)
+class SqlSuite extends FunSuite {
 
-    private val people = generatePeople()
-    private val peopleJson = generatePeopleJson()
+    private val people = generatePeople(100)
+    private val peopleJson = generatePeopleJson(100)
 
     test("create data frame from schema") {
         val schemaColumns = List("id", "name", "age", "city", "country")
@@ -65,7 +63,7 @@ class SqlSuite extends Sparky {
     }
 
     test("data frame show") {
-        val rdd = sc.parallelize(generatePeople(100)).map(p => (p.id, p.name, p.age, p.city, p.country))
+        val rdd = sc.parallelize(people).map(p => (p.id, p.name, p.age, p.city, p.country))
 
         val spark = SparkSession.builder().appName("example").getOrCreate()
         import spark.implicits._
